3ba5775c782b33d4bb1f4f3ddb3b48ae87573639
enaut-genua <enaut.genua@alumni.mondragon.edu>
2026-01-19 13:00:34 +0100

    Try fix mariadb

--- src/chassis/sql/database.py
+++ src/chassis/sql/database.py
@@ -13,14 +13,19 @@ SQLALCHEMY_DATABASE_URL = os.getenv(
 Engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
     # connect_args={"check_same_thread": False},
-    echo=False
+    echo=False,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    pool_size=5,
+    max_overflow=10
 )
 
 SessionLocal = async_sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=Engine,
-    future=True
+    future=True,
+    expire_on_commit=False,
 )
 
 Base = declarative_base()
